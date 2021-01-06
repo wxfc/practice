a8f846749c68b43a92660de07a9765710f57d6ed
Wayne Colaco <wcolaco@icloud.com>
2021-01-06 11:44:09 +1100

    prac

---
+++ 060121/addUp.js
@@ -0,0 +1,16 @@
+// Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+function addUp(num) {
+  let startNum = 0;
+  for (let i=1; i<=num; i++) {
+    startNum += i;
+  };
+  return startNum
+};
+
+console.log(addUp(4));
+console.log(addUp(13));
+console.log(addUp(600));
+
+
+
