8df3a4c5a2b3238582a1d0e738f5bc5d21a898f8
Wayne Colaco <wcolaco@icloud.com>
2021-01-09 21:28:08 +1100

    prac

--- 090121/calcType.js
+++ 090121/calcType.js
@@ -1,11 +1,11 @@
-// function calcType(a,b,res) {
-  // if ((a+b) === res) {
+// function calcType(a, b, res) {
+  // if ((a + b) === res) {
   //   return "addition";
-  // } else if ((a-b) === res) {
+  // } else if ((a - b) === res) {
   //   return "subtraction";
-  // } else if ((a*b) === res) {
+  // } else if ((a * b) === res) {
   //   return "multiplication";
-  // } else if ((a/b) === res) {
+  // } else if ((a / b) === res) {
   //   return "division"
   // } else {
   //   return null
